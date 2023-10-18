ff5258f822b5013c7775ef43bab85c60e31c0284
Azumayuk1 <azumayuki@outlook.com>
2023-10-19 01:07:11 +0300

    Lab 2

---
+++ app/src/main/java/ru/mirea/mymireaproject/part2/PackageInfo.kt
@@ -0,0 +1,9 @@
+package ru.mirea.mymireaproject.part2
+
+data class PackageInfo (
+    val appName: String = "",
+    var packageName: String = "",
+    var versionName: String = "",
+    var versionCode: Int = 0,
+    var dataDirectory: String = "",
+    )
